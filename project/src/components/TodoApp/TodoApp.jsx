7d3b84b5066dcbea4f6044bc209e338418cb1389
chrohithkumar <119103850+chrohithkumar@users.noreply.github.com>
2023-08-25 11:48:50 +0530

    ToDoApp

---
+++ project/src/components/TodoApp/TodoApp.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+
+function TodoApp() {
+  const [task,settask] = useState('');
+  const [alltodos,setalltodos]=useState([]);
+
+
+  const submithandler=(e)=>{
+    e.preventDefault();
+    const updatedtodos=[...alltodos,task];
+    setalltodos(updatedtodos);
+    settask('');
+
+    
+  }
+
+  return (
+    <div>
+       <center>
+        <div className='card'>
+            <div className='card-body'>
+                <h5 className='card-title'>ToDo Management Applications</h5>
+                <form onSubmit={submithandler}>
+                    <input type='text' size='30' value={task} onChange={(e)=>settask(e.target.value)}></input>  &nbsp;&nbsp;
+                    <input type='submit' value="Add" ></input>
+                </form>
+
+             {alltodos.map((item,index)=>
+             <div key={index}> 
+             <div>
+              <h4>{item} &nbsp; <button>Delete</button></h4> 
+             </div>
+             </div>)}
+
+            </div>
+        </div>
+
+       </center>
+    </div>
+  )
+}
+
+
+export default TodoApp;
